5d795ea51396c4a810b1e2608a983e7bbc117377
zach-m <zachary-moix@pluralsight.com>
2023-06-25 02:27:24 -0600

    victory condition

---
+++ Assets/Environment/StopRitual.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using StarterAssets;
+
+public class StopRitual : MonoBehaviour
+{
+    [SerializeField] GameObject boss;
+    [SerializeField] float interactDistance = 10f;
+    [SerializeField] float portalFadeDuration = 2.0f;
+    [SerializeField] Canvas victoryCanvas;
+    [SerializeField] TextMeshPro closeText;
+    [SerializeField] FirstPersonController playerInputController;
+    
+    Transform player;
+
+    void Start() {
+        player = FindObjectOfType<PlayerHealth>().transform;
+        playerInputController = player.GetComponent<FirstPersonController>();
+        victoryCanvas.enabled = false;
+        closeText.enabled = false;
+    }
+
+    void Update()
+    {
+        if (boss.GetComponent<EnemyHealth>().IsDead){
+            if (Vector3.Distance(transform.position, player.position) < interactDistance) {
+                closeText.enabled = true;
+                if (Input.GetKeyDown(KeyCode.E)) {
+                    closeText.enabled = false;
+                    StartCoroutine(ClosePortal());
+                }
+            }
+        }
+    }
+
+    private IEnumerator ClosePortal()
+    {
+        foreach (ParticleSystem part in GetComponentsInChildren<ParticleSystem>()) {
+            part.Stop();
+        }
+        yield return new WaitForSeconds(portalFadeDuration);
+        victoryCanvas.enabled = true;
+        
+    }
+}
